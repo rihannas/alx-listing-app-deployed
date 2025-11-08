97b7f3b490da031fe0876786fbb1eb414c179b4c
rihanas <rihanna.socials@gmail.com>
2025-11-09 02:13:27 +0300

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false, // disables Turbopack
-  },
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
