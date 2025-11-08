0dd8d7ab2d84a1b663b3bd8242e6dab0120b67d9
rihanas <rihanna.socials@gmail.com>
2025-11-09 02:11:32 +0300

    fix issue

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import PropertyDetail from './property/PropertyDetail';
+import PropertyDetail from './PropertyDetail';
 import { PropertyProps } from '@/interfaces';
 
 export default function PropertyDetailPage() {
