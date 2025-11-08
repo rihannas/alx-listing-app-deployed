1568a2398f40dd1a2c5f1eed220bd77f3120e3ec
rihanas <rihanna.socials@gmail.com>
2025-11-09 01:35:09 +0300

    fix type

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,7 +218,7 @@ const Home: React.FC = () => {
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
             {filteredProperties.map((property, index) => (
               <PropertyCard
-                key={property.id || index}
+                key={index}
                 property={property}
               />
             ))}
